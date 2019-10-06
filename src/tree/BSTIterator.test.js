9dad41ef9fbef07f0fb3507883e04118d7b2985a
niechao <chao.nie@starwin.com>
2019-10-06 14:10:12 +0800

    kthLarge

--- src/tree/BSTIterator.test.js
+++ src/tree/BSTIterator.test.js
@@ -10,16 +10,15 @@ describe("BSTIterator", () => {
   test("with queue", () => {
     let t = binaryTreeLayerGenerator([7, 3, 15, null, null, 9, 20]);
     let it = new BSTIterator(t);
-    console.log(it.data)
     expect(it.data).toEqual([3, 7, 9, 15, 20]);
-    // expect(it.next()).toEqual(3);
-    // expect(it.next()).toEqual(7);
-    // expect(it.hasNext()).toEqual(true);
-    // expect(it.next()).toEqual(9);
-    // expect(it.hasNext()).toEqual(true);
-    // expect(it.next()).toEqual(15);
-    // expect(it.hasNext()).toEqual(true);
-    // expect(it.next()).toEqual(20);
-    // expect(it.hasNext()).toEqual(false);
+    expect(it.next()).toEqual(3);
+    expect(it.next()).toEqual(7);
+    expect(it.hasNext()).toEqual(true);
+    expect(it.next()).toEqual(9);
+    expect(it.hasNext()).toEqual(true);
+    expect(it.next()).toEqual(15);
+    expect(it.hasNext()).toEqual(true);
+    expect(it.next()).toEqual(20);
+    expect(it.hasNext()).toEqual(false);
   });
 });
