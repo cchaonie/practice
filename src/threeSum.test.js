4fcfb2731c22af5481421ee0bf7b91966741c4d0
niechao <chao.nie@starwin.com>
2019-10-10 16:49:19 +0800

    threeSum

--- src/threeSum.test.js
+++ src/threeSum.test.js
@@ -3,7 +3,6 @@ const { threeSum } = require("./threeSum");
 describe("threeSum", () => {
   test("[-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0]", () => {
     const result = threeSum([-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0]);
-    console.log(result)
     expect(result).toEqual([
       [-5,1,4],[-4,0,4],[-4,1,3],[-2,-2,4],[-2,1,1],[0,0,0]
     ]);
