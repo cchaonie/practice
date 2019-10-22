9811fd44cba2be1ead34b344e85babf64ad7a0f2
fightant1w1ll <nc_8093@163.com>
2019-10-22 21:35:30 +0800

    change main

--- main/algorithm/kmpSearch.test.js
+++ main/algorithm/kmpSearch.test.js
@@ -3,7 +3,6 @@ const { next } = require("./kmpSearch");
 describe("kmpSearch", () => {
   test("next", () => {
     let result = next('abcdabde');
-    console.log(result);
     expect(result).toEqual([-1, 0, 0, 0, 0, 1, 2, 0]);
   })
 })
