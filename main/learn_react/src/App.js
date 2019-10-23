142205867f6050e257037f6635a760aea18b61fc
fightant1w1ll <nc_8093@163.com>
2019-10-23 22:54:00 +0800

    portal test

---
+++ main/learn_react/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Modal from './Modal.js';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div onClick={() => console.log('my descent is clicked')}>
+        <h1>Portal</h1>
+        <Modal></Modal>
+      </div>
+    )
+  }
+}
