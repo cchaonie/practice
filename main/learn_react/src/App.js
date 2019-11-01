0c95b6ff35b3fbaddaf3e05ac491ca92d1db3464
fightant1w1ll <nc_8093@163.com>
2019-11-01 10:46:21 +0800

    webpack source code

--- main/learn_react/src/App.js
+++ main/learn_react/src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import Modal from './Modal.js';
-import Store from './store';
+import store from './store';
+import Lazy from './Lazy'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false
+    }
+  }
+ 
   render() {
     return (
       <div onClick={() => console.log('my descent is clicked')}>
         <h1>Portal</h1>
         <Modal></Modal>
+        <button onClick={() => this.setState({ visible: !this.state.visible })}>toggle</button>
+        {
+          this.state.visible ? <Lazy></Lazy> : null
+        }
       </div>
     )
   }
 }
 
-Store.dispatch({
+store.dispatch({
   type: 'A',
   payload: { count: 100 }
 })
--- main/learn_react/src/store.js
+++ main/learn_react/src/store.js
@@ -22,51 +22,26 @@ const initialState = {
   conut: 1
 }
 
-const loggerA = function() {
-  return ({
-    getState
-  }) => {
-    console.log("loggerA: in getState")
-    return next => {
-      console.log("loggerA: in next")
-      return action => {
-        console.log("loggerA: in action")
-        console.log("loggerA:", getState())
-        next(action)
-      }
+const loggerA = function ({ getState }) {
+  return next => {
+    console.log("loggerA", next)
+    return action => {
+      console.log("loggerA", action)
+      // console.log("loggerA", getState())
+      next(action)
     }
   }
 }
 
-const loggerB = function() {
-  return ({
-    getState
-  }) => {
-    console.log("loggerB: in getState")
-    return next => {
-      console.log("loggerB: in next")
-      return action => {
-        console.log("loggerB: in action")
-        console.log("loggerB:", getState())
-        next(action)
-      }
+const loggerB = function ({ getState }) {
+  return next => {
+    console.log("loggerB", next)
+    return action => {
+      // console.log("loggerB", getState())
+      console.log("loggerB", action)
+      next(action)
     }
   }
 }
-
-function compose(...funcs) {
-  if (funcs.length === 0) {
-    return arg => arg
-  }
-
-  if (funcs.length === 1) {
-    return funcs[0]
-  }
-
-  return funcs.reduce((a, b) => (...args) => a(b(...args)))
-}
-
-console.log(compose(loggerA, loggerB))
-console.log(applyMiddleware(loggerA, loggerB))
 
 export default createStore(reducer, initialState, applyMiddleware(loggerA, loggerB));
