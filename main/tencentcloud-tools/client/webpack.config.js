9de1a4d074ac84fc907c414760727f1954bb98c6
niechao <cnie@trip.com>
2020-04-09 17:38:00 +0800

    upload image to server

--- main/tencentcloud-tools/client/webpack.config.js
+++ main/tencentcloud-tools/client/webpack.config.js
@@ -1,42 +1,42 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 // const CopyWebpackPlugin = require('copy-webpack-plugin');
 const isProd = process.env.NODE_ENV == "prod";
 
 module.exports = {
-  entry: path.join(__dirname, './src/index.js'),
+  entry: path.join(__dirname, "./src/index.js"),
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  mode: "development",
+  mode: isProd ? "production" : "development",
   // resolve: {
   //   extensions: ['js', 'jsx']
   // },
-  devtool: 'inline-source-map',
+  devtool: isProd ? false : "inline-source-map",
   module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/,
         // include: path.join(__dirname, 'src'),
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(js|jsx)/,
-        exclude: path.join(__dirname, 'node_modules'),
-        use: ['babel-loader']
+        exclude: path.join(__dirname, "node_modules"),
+        use: ["babel-loader"],
       },
       {
         test: /\.(ttf|eot|svg|woff|woff2)(\?.+)?$/,
-        loader: 'file-loader?name=[hash:12].[ext]'
-      }
-    ]
+        loader: "file-loader?name=[hash:12].[ext]",
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, './src/index.html')
+      template: path.join(__dirname, "./src/index.html"),
     }),
     // new CopyWebpackPlugin([
     //   { from: path.join(__dirname, './src/assets'), to: path.join(__dirname, 'dist') }
@@ -44,6 +44,12 @@ module.exports = {
   ],
   devServer: {
     contentBase: path.join(__dirname, "dist"),
-    port: 9000
-  }
-}
+    port: 9000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8181",
+        pathRewrite: { "^/api": "" },
+      },
+    },
+  },
+};
