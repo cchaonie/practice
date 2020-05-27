29e88c3254df85c38d2b2a4dacd6b6c7454a0cd2
niechao <cnie@trip.com>
2020-05-27 16:13:01 +0800

    nQueens problem

---
+++ main/algorithm/test/index.test.js
@@ -0,0 +1,16 @@
+const { permute } = require("../src/index");
+
+describe("permute", () => {
+  it("should pass", () => {
+    expect(permute([1, 2, 3])).toEqual([
+      [1, 2, 3],
+      [1, 3, 2],
+      [2, 1, 3],
+      [2, 3, 1],
+      [3, 1, 2],
+      [3, 2, 1],
+    ]);
+  });
+});
+
+
