a7afb55bb5902a7dc0f1923b61fae65808479bc0
fightant1w1ll <nc_8093@163.com>
2020-07-05 12:48:57 +0800

    longestValidParentheses

--- main/algorithm/src/other/dynamicProgramming/longestValidParentheses.js
+++ main/algorithm/src/other/dynamicProgramming/longestValidParentheses.js
@@ -2,20 +2,30 @@
  * @param {string} s
  * @return {number}
  */
-var longestValidParentheses = function(s) {
-    if (!s.length) return 0;
-    const stack = [];
-    let count = 0;
-    for (let i = 0; i < s.length; i++) {
-        if (s.charAt(i) === '(') {
-            stack.push(s.charAt(i));
+var longestValidParentheses = function (s) {
+  if (!s) return 0;
+  let dp = [0];
+  let result = 0;
+  for (let i = 1; i < s.length; i++) {
+    if (s.charAt(i) === "(") {
+      dp[i] = 0;
+    } else {
+      if (s.charAt(i - 1) === "(") {
+        dp[i] = i > 2 ? dp[i - 2] + 2 : 2;
+      } else {
+        if (s.charAt(i - dp[i - 1] - 1) === "(") {
+          dp[i] =
+            i - dp[i - 1] - 2 > 0
+              ? dp[i - dp[i - 1] - 2] + dp[i - 1] + 2
+              : dp[i - 1] + 2;
+        } else {
+          dp[i] = 0;
         }
-        if (stack.length && s.charAt(i) === ')') {
-            stack.pop();
-            count++;
-        }
+      }
     }
-    return count * 2;
+    result = Math.max(result, dp[i]);
+  }
+  return result;
 };
 
 module.exports = longestValidParentheses;
--- main/algorithm/test/other/dynamicProgramming/longestValidParentheses.test.js
+++ main/algorithm/test/other/dynamicProgramming/longestValidParentheses.test.js
@@ -4,4 +4,13 @@ describe("longestValidParentheses", () => {
   it("should pass", () => {
     expect(longestValidParentheses(")()())")).toEqual(4);
   });
+  it("should pass", () => {
+    expect(longestValidParentheses("(()")).toEqual(2);
+  });
+  it.only("should pass", () => {
+    expect(longestValidParentheses("(()())")).toEqual(6);
+  });
+  it("should pass", () => {
+    expect(longestValidParentheses( "()(()")).toEqual(2);
+  });
 });
