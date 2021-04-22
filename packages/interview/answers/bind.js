04a9a7b87ee8fb3454d2b995f5b3c8d00bcac793
Chris Nie <cnie@travix.com>
2021-04-22 12:41:03 +0800

    graphql cucumber

--- packages/interview/answers/bind.js
+++ packages/interview/answers/bind.js
@@ -3,3 +3,15 @@ function bind(fn, thisObj) {
         return fn.apply(thisObj, args);
     };
 }
+
+Function.prototype.ownCall = function (context, ...args) {
+    context = typeof context === "object" ? context : window;
+    // 防止覆盖掉原有属性
+    const key = Symbol();
+    // 这里的this为需要执行的方法
+    context[key] = this;
+    // 方法执行
+    const result = context[key](...args);
+    delete context[key];
+    return result;
+};
