beaf34bca1852940b0b55688b28034a851cd3fad
fightant1w11 <nc_8093@163.com>
2021-05-28 07:48:21 +0800

    concurrentPromiseScheduler

---
+++ packages/interview/answers/promise/concurrentPromise.js
@@ -0,0 +1,55 @@
+// JS实现一个带并发限制的异步调度器Scheduler，保证同时运行的任务最多有两个。完善代码中Scheduler类，使得以下程序能正确输出
+
+class Scheduler {
+    constructor(limit = 2) {
+        this.limit = limit;
+        this.runningCount = 0;
+        this.penddingPromiseCreator = [];
+    }
+
+    add(promiseCreator) {
+        const addResult = new Promise((resolve, reject) => {
+            this.runOrPend(promiseCreator, resolve);
+        });
+        return addResult;
+    }
+
+    runOrPend(promiseCreator, resolve) {
+        if (this.runningCount < this.limit) {
+            const p = promiseCreator();
+            this.runningCount++;
+            p.then(() => {
+                this.runningCount--;
+                resolve();
+                const pending = this.penddingPromiseCreator.shift();
+                if (pending) {
+                  this.runOrPend(...pending);
+                }
+            });
+        } else {
+            this.penddingPromiseCreator.push([promiseCreator, resolve]);
+        }
+    }
+}
+
+const timeout = time =>
+    new Promise(resolve => {
+        setTimeout(resolve, time);
+    });
+
+const scheduler = new Scheduler();
+const addTask = (time, order) => {
+    scheduler.add(() => timeout(time)).then(() => console.log(order));
+};
+
+addTask(1000, "1");
+addTask(500, "2");
+addTask(300, "3");
+addTask(400, "4");
+// output: 2 3 1 4
+
+// 一开始，1、2两个任务进入队列
+// 500ms时，2完成，输出2，任务3进队
+// 800ms时，3完成，输出3，任务4进队
+// 1000ms时，1完成，输出1
+// 1200ms时，4完成，输出4
