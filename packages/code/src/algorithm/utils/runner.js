c4ef0d22d959cb6ae73ba285e505ccdf8165865c
Chris Nie <nc_8093@163.com>
2022-10-27 14:08:19 +0800

    Fix priorityQueue and test case

--- packages/code/src/algorithm/utils/runner.js
+++ packages/code/src/algorithm/utils/runner.js
@@ -1,12 +1,12 @@
 exports.run = function (obj, methods, parameters) {
-    for (let i = 0; i < methods.length; i += 1) {
-        try {
-            const ret = obj[methods[i]](...parameters[i]);
-            if (ret) {
-                console.log(ret);
-            }
-        } catch (error) {
-            console.error(error);
-        }
+  for (let i = 0; i < methods.length; i += 1) {
+    try {
+      const ret = obj[methods[i]](...parameters[i]);
+      if (ret) {
+        console.log(ret);
+      }
+    } catch (error) {
+      console.error(error);
     }
+  }
 };
