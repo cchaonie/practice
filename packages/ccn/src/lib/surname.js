cb8f688faeaefe7553aa1407525fdbda8ebdcfc2
Chris Nie <cnie@travix.com>
2024-01-23 19:42:26 +0800

    Chore

--- packages/ccn/src/lib/surname.js
+++ packages/ccn/src/lib/surname.js
@@ -1,8 +1,8 @@
-const flatten = require("flatten");
-const random = require("random-to");
-const surnameDict = require("../dict/surnameDict");
+const flatten = require('flatten');
+const random = require('random-to');
+const surnameDict = require('../dict/surnameDict');
 
-const dict = surnameDict.split("\n\n");
+const dict = surnameDict.split('\n\n');
 
 const weights = [100, 70, 10, 5, 1, 1];
 
@@ -10,33 +10,33 @@ let w = 0;
 let idx = 0;
 
 const surnames = dict.reduce((acc, names) => {
-    names = names.split("\n");
-    names = flatten(
-        names.map(n => {
-            return n.split(" ");
-        })
-    );
+  names = names.split('\n');
+  names = names
+    .map(n => {
+      return n.split(' ');
+    })
+    .flat();
 
-    if (names.length === 1 && names[0] === "") return acc;
-    for (var i = 0; i < names.length; i++) {
-        acc.push({
-            name: names[i],
-            min: w,
-            max: w + weights[idx] - 1,
-        });
+  if (names.length === 1 && names[0] === '') return acc;
+  for (var i = 0; i < names.length; i++) {
+    acc.push({
+      name: names[i],
+      min: w,
+      max: w + weights[idx] - 1,
+    });
 
-        w += weights[idx];
-    }
+    w += weights[idx];
+  }
 
-    idx++;
-    return acc;
+  idx++;
+  return acc;
 }, []);
 
 const count = surnames[surnames.length - 1].max;
 
 exports.getOne = () => {
-    const idx = random.from0upto(count);
-    return surnames.find(name => {
-        return name.min <= idx && name.max >= idx;
-    }).name;
+  const idx = random.from0upto(count);
+  return surnames.find(name => {
+    return name.min <= idx && name.max >= idx;
+  }).name;
 };
