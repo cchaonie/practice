33ed4a190c25ca5898f9fe01ad96c5fc2b5ac003
Chris Nie <nc_8093@163.com>
2024-01-27 17:27:30 +0800

    Move isomorphic to separate project

--- packages/isomorphic/src/client/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { hydrateRoot } from 'react-dom/client';
-
-import App from '..';
-
-const root = document.getElementById('root');
-
-hydrateRoot(root, <App />);
--- packages/isomorphic/src/server/utils/index.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export const getManifest = (type: string, filename: string) => {
-    const content = fs.readFileSync(
-        path.resolve(process.cwd(), `dist/${type}/${filename}.json`)
-    );
-    return JSON.parse(content.toString());
-};
