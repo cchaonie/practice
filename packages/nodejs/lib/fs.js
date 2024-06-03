ff0b06bd18058fe217a9f3221fed5313b5b20d9f
chris nie <cnie@trip.com>
2024-06-03 17:43:48 +0800

    learn nodejs

---
+++ packages/nodejs/lib/fs.js
@@ -0,0 +1,10 @@
+import { writeFile } from 'node:fs';
+import { Buffer } from 'node:buffer';
+
+const data = new Uint8Array(
+  Buffer.from('Hello Node.js this is the file content!')
+);
+writeFile('message.txt', data, err => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
